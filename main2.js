eaf3f80c7c7a12ecf0cc61ae1ddb9193a2174d6a
harukaweb <95006378+harukaweb@users.noreply.github.com>
2022-06-17 17:01:13 +0900

    Update main2.js

--- main2.js
+++ main2.js
@@ -1,1 +1,42 @@
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
 
+function randomValueFromArray(array){
+  const random = Math.floor(Math.random()*array.length);
+  return array[random];
+}
+
+const storyText = '名前ちゃん:inserte::insertx:！:inserty:！:insertz::inserte:';
+const insertX = ['愛しいなぁもう', 'どうしちゃったノカナ', 'オハヨウ〜'];
+const insertY = ['なんちゃッテ', '僕は、すごく心配だよ', '今日はどんな一日だった？'];
+const insertZ = ['仕事が早く終わりそうなんだけど、ご飯でもどうかな',
+                 'ホント可愛すぎだよ〜マッタクもう',
+                 '明日も仕事だけどなかなか寝れないよ〜',
+                 '学校をサボるなんて、悪い子だなぁ'];
+const insertE = ['😍', '👍', '❗️', '🎵', '(^_^)', '(^o^)', '(^з<)', '(笑)'];
+
+randomize.addEventListener('click', result);
+
+function result() {
+  let newStory = storyText;
+
+  const xItem = randomValueFromArray(insertX);
+  const yItem = randomValueFromArray(insertY);
+  const zItem = randomValueFromArray(insertZ);
+  const eItem = randomValueFromArray(insertE);
+
+  newStory = newStory.replace(':inserte:',eItem);
+  newStory = newStory.replace(':insertx:',xItem);
+  newStory = newStory.replace(':inserty:',yItem);
+  newStory = newStory.replace(':insertz:',zItem);
+  newStory = newStory.replace(':inserte:',eItem);
+
+  if (customName.value !== '') {
+    const name = customName.value;
+    newStory = newStory.replace('名前', name);
+  }
+
+  story.textContent = newStory;
+  story.style.visibility = 'visible';
+}

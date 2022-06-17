7e76d88c683a0487c48e39bc2423c0f6b78cc184
miwume <95006778+miwume@users.noreply.github.com>
2022-06-17 13:12:36 +0900

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function randomValueFromArray(array){
 const storyText = ':insertx::inserty::insertz:';
 const insertX = ['なんちゃッテ', 'どうしちゃったノカナ', 'オハヨウ〜'];
 const insertY = ['愛しいなぁもう', '僕は、すごく心配だよ', '今日はどんな一日だった？'];
-const insertZ = ['仕事が早く終わりそうなんだけど、ご飯でもどうかな', 'ホント可愛すぎだよ〜マッタクもう', 'に気づかずに楽しげに歌いながら'];
+const insertZ = ['仕事が早く終わりそうなんだけど、ご飯でもどうかな', 'ホント可愛すぎだよ〜マッタクもう', '明日も仕事だけどなかなか寝れないよ〜'];
 
 randomize.addEventListener('click', result);
 

39a0f8789f89ac7701c2f28fb1f1c68315f97f6c
miwume <95006778+miwume@users.noreply.github.com>
2022-06-24 05:13:13 +0900

    Update main.js

--- main.js
+++ main.js
@@ -42,3 +42,38 @@ function result() {
   story.textContent = newStory;
   story.style.visibility = 'visible';
 }
+
+const onClickAdd = () => {
+  const textEl = document.getElementById("customname");
+
+  const text = textEl.value;
+
+  textEl.value = "";
+
+  const li = document.createElement("li");
+
+  const div = document.createElement("div");
+
+  const p = document.createElement("p");
+  p.textContent = text;
+
+  const button = document.createElement("button");
+  button.textContent = "削除";
+
+  button.addEventListener("click", () => {
+    const deleteTarget = button.closest("li");
+
+    document.getElementById("memo-list").removeChild(deleteTarget);
+  });
+
+  div.appendChild(p);
+  div.appendChild(button);
+
+  li.appendChild(div);
+
+  document.getElementById("memo-list").appendChild(li);
+};
+
+document
+  .getElementById("randomize")
+  .addEventListener("click", () => onClickAdd());
